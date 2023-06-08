15a8c3c1365e6c314f01bc0cbdea070e85e36e21
Badrul Akbar A M <badrl.xrpl2017smkrus@gmail.com>
2023-06-08 16:46:15 +0700

    compose status field

--- helper/index.js
+++ helper/index.js
@@ -2,6 +2,7 @@
 module.exports.compose = function(handler, status, message, payload = null){
     return handler.response({
         SENDER: "FRUTIFY BACKEND APP",
+        STATUS: status,
         MESSAGE: message,
         PAYLOAD: payload
     })
