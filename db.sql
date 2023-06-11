078c4ce1e9a75341652ecf0140fcd96542648ed9
Badrul Akbar A M <badrl.xrpl2017smkrus@gmail.com>
2023-06-11 15:36:36 +0700

    udpate .sql

--- db.sql
+++ db.sql
@@ -24,9 +24,11 @@ CREATE TABLE IF NOT EXISTS `fruit` (
   `FRUIT_ID` int(11) NOT NULL AUTO_INCREMENT,
   `FRUIT_NAME` varchar(50) NOT NULL,
   PRIMARY KEY (`FRUIT_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table frutify.fruit: ~0 rows (approximately)
+-- Dumping data for table frutify.fruit: ~1 rows (approximately)
+INSERT INTO `fruit` (`FRUIT_ID`, `FRUIT_NAME`) VALUES
+	(1, 'Apel');
 
 -- Dumping structure for table frutify.product
 CREATE TABLE IF NOT EXISTS `product` (
@@ -36,12 +38,16 @@ CREATE TABLE IF NOT EXISTS `product` (
   `PRODUCT_NAME` varchar(50) NOT NULL,
   `PRODUCT_DESCRIPTION` text NOT NULL,
   `PRODUCT_PRICE` int(11) NOT NULL,
+  `PRODUCT_UNIT` varchar(50) NOT NULL,
   `PRODUCT_QUALITY` varchar(50) NOT NULL,
   `PRODUCT_FILE_PATH` varchar(50) NOT NULL,
   PRIMARY KEY (`PRODUCT_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table frutify.product: ~0 rows (approximately)
+-- Dumping data for table frutify.product: ~2 rows (approximately)
+INSERT INTO `product` (`PRODUCT_ID`, `FRUIT_ID`, `USER_ID`, `PRODUCT_NAME`, `PRODUCT_DESCRIPTION`, `PRODUCT_PRICE`, `PRODUCT_UNIT`, `PRODUCT_QUALITY`, `PRODUCT_FILE_PATH`) VALUES
+	(1, 1, 1, 'Apel Segar', 'Apel segar kualitas debest', 30000, 'kg', 'GOOD', '-'),
+	(2, 1, 1, 'Apel Segar', 'Apel segar kualitas lumayan', 25000, 'kg', 'GOOD', '-');
 
 -- Dumping structure for table frutify.user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -50,15 +56,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   `USER_PHONE` varchar(50) NOT NULL,
   `USER_PASSWORD` varchar(50) NOT NULL,
   `USER_FULLNAME` varchar(50) NOT NULL,
+  `USER_ROLE` varchar(10) NOT NULL,
   `USER_ADDRESS` text NOT NULL,
-  `USER_TOKEN` varchar(50) NOT NULL,
-  `USER_TOKEN_EXPIRED` varchar(50) NOT NULL,
+  `USER_TOKEN` varchar(255) NOT NULL,
+  `USER_TOKEN_EXPIRED` varchar(30) NOT NULL,
   PRIMARY KEY (`USER_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Dumping data for table frutify.user: ~1 rows (approximately)
-INSERT INTO `user` (`USER_ID`, `USER_EMAIL`, `USER_PHONE`, `USER_PASSWORD`, `USER_FULLNAME`, `USER_ADDRESS`, `USER_TOKEN`, `USER_TOKEN_EXPIRED`) VALUES
-	(1, 'badrulcr5@gmail.com', '081215992673', '12345678', 'Badrul Akbar A M', '', '', '');
+-- Dumping data for table frutify.user: ~2 rows (approximately)
+INSERT INTO `user` (`USER_ID`, `USER_EMAIL`, `USER_PHONE`, `USER_PASSWORD`, `USER_FULLNAME`, `USER_ROLE`, `USER_ADDRESS`, `USER_TOKEN`, `USER_TOKEN_EXPIRED`) VALUES
+	(1, 'badrul@gmail.com', '081215992673', '12345678', 'Badrul Akbar A M', 'SELLER', '', '', ''),
+	(3, 'akbar@gmail.com', '08122345', '12345678', 'Akbar M', 'BUYER', '', '', '');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
